940b2b03618419f0a5aed8a2b3656d9490e1edbd
Sujith <sujithchandraj09@gmail.com>
2026-01-28 21:22:48 +0530

    Updated website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import github from "@astrojs/github";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://SujithJ05.github.io/FoxePro/",
+  output: "static",
+  adapter: github(),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
